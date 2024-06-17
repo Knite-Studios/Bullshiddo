ab3b8e0933d034913171d1d7a439c60810625ad2
Percy <perzeus.ttv@gmail.com>
2024-06-17 03:50:44 -0600

    eepy

---
+++ Assets/Runtime/Gameplay/UI/TrackUI.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+
+namespace Oculus.Interaction.Bullshiddo
+{
+    /// <summary>
+    /// Displays saved player data about the track.
+    /// </summary>
+    public class TrackUI : MonoBehaviour
+    {
+        [Header("ScoreCard Components")]
+        [SerializeField]
+        private TextMeshProUGUI _topScoreText;
+
+        [SerializeField]
+        private TextMeshProUGUI _topComboText;
+
+        [SerializeField]
+        private TextMeshProUGUI _topSpeedText;
+
+        [SerializeField]
+        private TextMeshProUGUI _attemptsText;
+
+        public string TrackID
+        {
+            get => _trackID;
+            set
+            {
+                _trackID = value;
+                UpdateSongHighScore();
+            }
+        }
+
+        private string _trackID;
+
+        private void UpdateSongHighScore()
+        {
+            string songSelection = _trackID;
+            _topScoreText.SetText(Store.GetInt($"score-{songSelection}").ToString());
+            _topComboText.SetText(Store.GetInt($"combo-{songSelection}").ToString());
+            _topSpeedText.SetText(Store.GetFloat($"speed-{songSelection}").ToString("N1"));
+            _attemptsText.SetText(Store.GetInt($"attempts-{songSelection}").ToString());
+        }
+    }
+}
