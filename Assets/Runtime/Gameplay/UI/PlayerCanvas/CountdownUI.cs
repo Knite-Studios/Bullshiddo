f559760c63db6427a50b1222750f91638947185e
Percy <perzeus.ttv@gmail.com>
2024-06-17 03:46:34 -0600

    feat: Combo and Countdown

---
+++ Assets/Runtime/Gameplay/UI/PlayerCanvas/CountdownUI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Oculus.Interaction.Bullshiddo
+{
+    /// <summary>
+    /// Handles the UI that appears before a track starts.
+    /// </summary>
+    public class CountdownUI : ActiveStateObserver, IActiveState
+    {
+        static readonly WaitForSeconds _oneSecond = new WaitForSeconds(1);
+
+        [SerializeField]
+        private GameObject _countdownParent;
+
+        [SerializeField]
+        private GameObject _countdownIcon;
+
+        [SerializeField]
+        private TextMeshProUGUI _countdownText;
+
+        [SerializeField]
+        private int _countdown;
+
+        private bool _countingDown;
+
+        bool IActiveState.Active => _countingDown;
+
+        protected override void HandleActiveStateChanged()
+        {
+            if (Active) StartCountdown();
+        }
+
+        private void StartCountdown()
+        {
+            StartCoroutine(CountdownRoutine());
+        }
+
+        private IEnumerator CountdownRoutine()
+        {
+            _countingDown = true;
+            _countdownParent.SetActive(true);
+            for (int i = _countdown; i > 0; i--)
+            {
+                _countdownText.SetText(i.ToString());
+                yield return _oneSecond;
+            }
+            _countdownText.SetText("GO");
+            yield return _oneSecond;
+            _countdownParent.SetActive(false);
+            _countingDown = false;
+        }
+    }
+}
---
+++ Assets/Runtime/Gameplay/UI/PlayerCanvas/ScoreCardUI.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+
+namespace Oculus.Interaction.Bullshiddo
+{
+    /// <summary>
+    /// Handles the UI for the score/combo card.
+    /// </summary>
+    public class ScoreCardUI : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField]
+        private TextMeshProUGUI _totalScoreText;
+
+        [SerializeField]
+        private TextMeshProUGUI _comboScoreText;
+
+        [SerializeField]
+        private TextMeshProUGUI _multiplierText;
+
+        [SerializeField]
+        private TextMeshProUGUI _multiplierWordText;
+
+        [Header("References")]
+        [SerializeField]
+        private ScoreKeeper _scoreKeeper;
+
+        private void Awake()
+        {
+            _scoreKeeper.WhenChanged += UpdateScoreCardUI;
+            UpdateScoreCardUI();
+        }
+
+        private void UpdateScoreCardUI()
+        {
+            _totalScoreText.SetText(_scoreKeeper.Score.ToString());
+            _comboScoreText.SetText(_scoreKeeper.HitsInARow.ToString());
+            _multiplierText.SetText($"x{_scoreKeeper.Combo}");
+            UpdateMultiplierWord();
+        }
+
+        private void UpdateMultiplierWord()
+        {
+            if (_scoreKeeper.Combo < 10)
+                _multiplierWordText.SetText("");
+            else if (_scoreKeeper.Combo >= 20)
+                _multiplierWordText.SetText("Amazing!");
+            else if (_scoreKeeper.Combo >= 15)
+                _multiplierWordText.SetText("Keep Going");
+            else if (_scoreKeeper.Combo >= 10)
+                _multiplierWordText.SetText("Awesome!");
+        }
+    }
+}
