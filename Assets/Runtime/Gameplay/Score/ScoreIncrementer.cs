1468bde25387b42a3ea5ab2cf0be75c5a022362f
Percy <perzeus.ttv@gmail.com>
2024-06-17 03:42:39 -0600

    feat: score track
    reused. will expand

---
+++ Assets/Runtime/Gameplay/Score/ScoreIncrementer.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+namespace Oculus.Interaction.Bullshiddo
+{
+    /// <summary>
+    /// Increments the player's score and multiplies by velocity.
+    /// </summary>
+    public class ScoreIncrementer : MonoBehaviour
+    {
+        private static ScoreKeeper _score;
+
+        [Tooltip("When assigned, score will only be counted when the ActiveState is true.")]
+        [SerializeField, Optional]
+        private ReferenceActiveState _condition;
+
+        [SerializeField]
+        private HandHitDetector _hitDetector;
+        [SerializeField]
+        private bool _includeVelocity = true;
+        [SerializeField]
+        private float _metersPerSecondMultiplierThreshold = 8f;
+        [SerializeField]
+        private Spawner _scoreSpawner, _failSpawner;
+
+        public int LastScore { get; private set; }
+
+        private void Awake()
+        {
+            _hitDetector.WhenHitResolved += RegisterScore;
+        }
+
+        /// <summary>
+        /// Increments the score, optionally using velocity.
+        /// </summary>
+        private void RegisterScore()
+        {
+            if (_condition.HasReference && !_condition) return;
+            if (_score == null && (_score = FindObjectOfType<ScoreKeeper>()) == null) return;
+            if (_hitDetector.PoseWasCorrect)
+            {
+                LastScore = AddScore();
+                _scoreSpawner.Spawn();
+            }
+            else
+            {
+                _score.BreakCombo();
+                _failSpawner.Spawn();
+            }
+        }
+
+        private int AddScore()
+        {
+            if (_includeVelocity && _hitDetector.LastHand.TryGetAspect<RawHandVelocity>(out var velocityCalculator))
+            {
+                // Record the speed of the hit, to display on the results screen.
+                var speed = velocityCalculator.CalculateThrowVelocity(transform).LinearVelocity.magnitude;
+                _score.AddSpeed(speed);
+
+                // Add bonus multiplier based on speed.
+                var speedMultiplier = (int)(speed / _metersPerSecondMultiplierThreshold);
+                return _score.AddScore(speedMultiplier);
+            }
+            else
+            {
+                return _score.AddScore();
+            }
+        }
+    }
+}
---
+++ Assets/Runtime/Gameplay/Score/ScoreKeeper.cs
@@ -0,0 +1,96 @@
+using System;
+using UnityEngine;
+
+namespace Oculus.Interaction.Bullshiddo
+{
+    /// <summary>
+    /// Tracks the consecutive hits and their velocity to calculate the score.
+    /// </summary>
+    public class ScoreKeeper : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The number of consecutive hits required to up the score multiplier.")]
+        private int _comboIncrement = 4;
+
+        [SerializeField]
+        private int _scoreGenericMultiplier = 10;
+
+        private int _score = 0;
+        private int _combo = 0;
+        private int _hitsInARow = 0;
+        private int _hitsTotal = 0;
+        private int _hitsSuccessful = 0;
+
+        public event Action WhenChanged;
+
+        public int Score => _score;
+        public int HitsTotal => _hitsTotal;
+        public int HitsSuccessful => _hitsSuccessful;
+        public int Combo => (_combo / _comboIncrement) + 1;
+        public int HitsInARow => _hitsInARow;
+
+        public float TopSpeed { get; private set; }
+        public int TopHitsInARow { get; private set; }
+        public int TopMultiplier { get; private set; }
+
+        public void AddSpeed(float speed)
+        {
+            TopSpeed = Mathf.Max(TopSpeed, speed);
+        }
+
+        public int AddScore(int bonus)
+        {
+            int comboScore = ((_combo + (bonus * _scoreGenericMultiplier)) / _comboIncrement) + 1;
+            comboScore *= _scoreGenericMultiplier;
+            _score += comboScore;
+            _combo += bonus + 1;
+            _hitsInARow++;
+            _hitsSuccessful++;
+            TopHitsInARow = Math.Max(TopHitsInARow, _hitsInARow);
+            TopMultiplier = Math.Max(TopMultiplier, Combo);
+
+            WhenChanged?.Invoke();
+            return comboScore;
+        }
+
+        public int AddScore()
+        {
+            int comboScore = (_combo / _comboIncrement) + 1;
+            comboScore *= _scoreGenericMultiplier;
+            _score += comboScore;
+            _combo++;
+            _hitsInARow++;
+            _hitsSuccessful++;
+
+            TopHitsInARow = Math.Max(TopHitsInARow, _hitsInARow);
+            TopMultiplier = Math.Max(TopMultiplier, Combo);
+
+            WhenChanged?.Invoke();
+            return comboScore;
+        }
+
+        public void AddToTotalHits()
+        {
+            _hitsTotal++;
+        }
+
+        public void BreakCombo()
+        {
+            _combo = _combo - (_combo % _comboIncrement) - 1;
+            _hitsInARow = 0;
+            WhenChanged?.Invoke();
+        }
+
+        public void ResetScore()
+        {
+            _score = 0;
+            _combo = 0;
+            _hitsInARow = 0;
+            _hitsSuccessful = 0;
+            _hitsTotal = 0;
+            TopSpeed = 0;
+            TopMultiplier = 0;
+            TopHitsInARow = 0;
+            WhenChanged?.Invoke();
+        }
+    }
+}
